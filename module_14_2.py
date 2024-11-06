4a6e87c204b8fc6425eb1a63be3c99728a70d72f
frodg3 <statsuk.gosha@mail.ru>
2024-11-06 16:32:40 +1000

    Add files via upload

---
+++ module_14_2.py
@@ -0,0 +1,28 @@
+import sqlite3
+
+conn = sqlite3.connect('not_telegram.db')
+
+cursor = conn.cursor()
+
+cursor.execute('''
+DELETE FROM Users WHERE id = 6
+''')
+
+cursor.execute('''
+SELECT COUNT(*) FROM Users
+''')
+total_users = cursor.fetchone()[0]
+
+cursor.execute('''
+SELECT SUM(balance) FROM Users
+''')
+all_balances = cursor.fetchone()[0]
+
+if total_users > 0:
+    average_balance = all_balances / total_users
+else:
+    average_balance = 0
+
+print(average_balance)
+
+conn.close()
