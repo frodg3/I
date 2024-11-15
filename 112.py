7bb8c11af8f6df1073abaf88b4c01bc3bae80dcd
frodg3 <statsuk.gosha@mail.ru>
2024-11-15 11:16:54 +1000

    Add files via upload

---
+++ 112.py
@@ -0,0 +1,55 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.utils import executor
+import asyncio
+
+API_TOKEN = 'YOUR_TELEGRAM_BOT_TOKEN'
+
+bot = Bot(token= "7959603468:AAFXYwQhOLGNFXCKTaa5v-wskupio8A-Jbk")
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    await message.answer("Здравствуйте! Я бот помогающий вашему здоровью. Напишите 'Calories', чтобы начать подсчёт калорий.")
+
+@dp.message_handler(lambda message: message.text.lower() == 'calories')
+async def set_age(message: types.Message):
+    await message.answer("Введите свой возраст:")
+    await UserState.age.set()
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message: types.Message, state: FSMContext):
+    await state.update_data(age=message.text)  # Сохраняем возраст
+    await message.answer("Введите свой рост (в см):")
+    await UserState.growth.set()
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message: types.Message, state: FSMContext):
+    await state.update_data(growth=message.text)  # Сохраняем рост
+    await message.answer("Введите свой вес (в кг):")
+    await UserState.weight.set()
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message: types.Message, state: FSMContext):
+    await state.update_data(weight=message.text)  # Сохраняем вес
+    data = await state.get_data()
+
+    age = int(data.get('age'))
+    growth = int(data.get('growth'))
+    weight = int(data.get('weight'))
+
+    calories = 10 * weight + 6.25 * growth - 5 * age + 5
+
+    await message.answer(f"Ваша норма калорий: {calories:.2f} ккал.")
+    await state.finish()
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
